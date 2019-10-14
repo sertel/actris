a6825e8d8ec10ab568f4e6e88b9fcd78a47c2d18
jihgfee <jihgfee@gmail.com>
2019-10-14 13:37:51 -0400

    Bumped to work with Coq 8.10

--- theories/examples/sort.v
+++ theories/examples/sort.v
@@ -143,7 +143,7 @@ Section sort.
     wp_apply (start_chan_proto_spec sort_protocol_func); iIntros (c) "Hc".
     { rewrite -(right_id END%proto _ (iProto_dual _)).
       wp_apply (sort_service_func_spec with "Hc"); auto. }
-    wp_send with "[$Hcmp]".
+    wp_send (A I R) with "[$Hcmp]".
     wp_send with "[$Hl]".
     wp_recv (ys) as "(Hsorted & Hperm & Hl)".
     wp_pures. iApply "HΦ"; iFrame.
