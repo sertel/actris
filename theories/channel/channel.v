202a6080e169bcc6ee568c4193081ca42c22ea72
jihgfee <jihgfee@gmail.com>
2019-10-14 13:42:33 -0400

    Deleted moved Iris specific stuff

--- theories/channel/channel.v
+++ theories/channel/channel.v
@@ -67,15 +67,6 @@ Definition chanΣ : gFunctors :=
 Instance subG_chanΣ {Σ} : subG chanΣ Σ → chanG Σ.
 Proof. solve_inG. Qed.
 
-(** MOVE TO IRIS *)
-Global Instance fst_atomic a v1 v2 : Atomic a (Fst (v1,v2)%V).
-Proof.
-  apply strongly_atomic_atomic, ectx_language_atomic;
-    [inversion 1; naive_solver
-    |apply ectxi_language_sub_redexes_are_values; intros [] **; naive_solver].
-Qed.
-
-
 Section channel.
   Context `{!heapG Σ, !chanG Σ} (N : namespace).
 
