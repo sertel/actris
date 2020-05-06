85c15c6453ae31e4a536aafb6bb3fde60432ce31
jihgfee <jihgfee@gmail.com>
2020-05-06 23:59:14 +0200

    More clean up

--- theories/examples/subprotocols.v
+++ theories/examples/subprotocols.v
@@ -6,7 +6,7 @@ Section basics.
   Lemma reference_example (l2' : loc) :
     l2' ↦ #22 -∗
     (<! (l1 l2 : loc)> MSG (#l1, #l2) {{ l1 ↦ #20 ∗ l2 ↦ #22 }}; END)%proto ⊑
-      (<! (l1 : loc)> MSG (#l1, #l2') {{ l1 ↦ #20 }}; END)%proto.
-  Proof. iIntros "Hl2'" (l1) "Hl1". iExists l1, l2'. by iFrame. Qed.
+    (<! (l1 : loc)> MSG (#l1, #l2') {{ l1 ↦ #20 }}; END)%proto.
+  Proof. iIntros "Hl2'" (l1) "Hl1". iExists l1, l2'. by iFrame "Hl1 Hl2'". Qed.
 
 End pair.
