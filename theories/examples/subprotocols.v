946b911f8cede22acc7246f806eb7c37656a6dc8
jihgfee <jihgfee@gmail.com>
2020-05-06 23:52:49 +0200

    Mechanised paper example of location subprotocols

---
+++ theories/examples/subprotocols.v
@@ -0,0 +1,15 @@
+From actris.channel Require Import proofmode proto channel.
+
+Section basics.
+  Context `{heapG Σ, chanG Σ}.
+
+  Lemma reference_example (l2' : loc) :
+    l2' ↦ #22 -∗
+    (<! (l1 l2 : loc)> MSG (#l1, #l2) {{ l1 ↦ #20 ∗ l2 ↦ #22 }}; END)%proto ⊑
+      (<! (l1 : loc)> MSG (#l1, #l2') {{ l1 ↦ #20 }}; END)%proto.
+  Proof.
+    iIntros "Hl2'" (l1) "Hl1". iExists l1, l2'.
+    iSplitL; eauto with iFrame.
+  Qed.
+
+End pair.
