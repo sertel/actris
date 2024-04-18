3c659ddb34c8df0c0fade2ef94291e2575a80c86
Jonas Kastberg Hinrichsen <jihgfee@gmail.com>
2024-04-18 17:27:04 +0200

    Added program

--- multi_actris/examples/mpc.v
+++ multi_actris/examples/mpc.v
@@ -2,13 +2,82 @@ From iris.heap_lang Require Import adequacy.
 From iris.heap_lang.lib Require Import assert.
 From multi_actris.channel Require Import proofmode.
 
+Notation A := 0.
+Notation B := 1.
+Notation C := 2.
+Notation D := 3.
+
+
+Definition program : val :=
+  λ: <>,
+     let: "cs" := new_chan #4 in
+     let: "ch0" := get_chan "cs" #A in
+     let: "ch1" := get_chan "cs" #B in
+     let: "ch2" := get_chan "cs" #C in
+     let: "ch3" := get_chan "cs" #D in
+     (* A *)
+     Fork (let: "a" := #42 in
+           let: "a1" := #10 in
+           let: "a2" := #20 in
+           let: "a3" := #12 in
+           send "ch0" #B "a2";;
+           send "ch0" #C "a3";;
+           let: "b1" := recv "ch0" #B in
+           let: "c1" := recv "ch0" #C in
+           let: "s1" := "a1"+"b1"+"c1" in
+           send "ch0" #B "s1";;
+           send "ch0" #C "s1";;
+           let: "s2" := recv "ch0" #B in
+           let: "s3" := recv "ch0" #C in
+           send "ch0" #D ("s1"+"s2"+"s3"));;
+     (* B *)
+     Fork (let: "b" := #100 in
+           let: "b1" := #20 in
+           let: "b2" := #30 in
+           let: "b3" := #50 in
+           (* <(Recv,A) @ (a2:Z)> MSG #a2 ; <(Send,A) @ (b1:Z)> MSG #b1 ; *)     
+           let: "a2" := recv "ch1" #A in
+           send "ch1" #A "b1";;
+           (* <(Send,C) @ (b3:Z)> MSG #b3 ; <(Recv,C) @ (c2:Z)> MSG #c2 ; *)
+           send "ch1" #C "b3";;
+           let: "c2" := recv "ch1" #C in
+           (* <(Recv,A) @ (s1:Z)> MSG #s1 ; *)
+           let: "s1" := recv "ch1" #A in
+           (* <(Send,A) @ (s2:Z) (b2:Z)> MSG #s2 {{ ⌜(s2 = a2 + b2 + c2)%Z⌝ }} ; *)
+           let: "s2" := "a2"+"b2"+"c2" in
+           send "ch1" #A "s2";;
+           (* <(Send,C)> MSG #s2 ; <(Recv,C) @ (s3:Z)> MSG #s3 ; *)
+           send "ch1" #C "s2";;
+           let: "s3" := recv "ch1" #C in
+           send "ch1" #D ("s1"+"s2"+"s3"));;
+     Fork (let: "c" := #7 in
+           let: "c1" := #1 in
+           let: "c2" := #2 in
+           let: "c3" := #4 in
+           (* <(Recv,A) @ (a3:Z)> MSG #a3 ; <(Send,A) @ (c1:Z)> MSG #c1 ; *)
+           let: "a3" := recv "ch2" #A in
+           send "ch2" #A "c1";;
+           (* <(Recv,B) @ (b3:Z)> MSG #b3 ; <(Send,B) @ (c2:Z)> MSG #c2 ; *)
+           let: "b3" := recv "ch2" #B in
+           send "ch2" #B "c2";;
+           (* <(Recv,A) @ (s1:Z)> MSG #s1 ; *)
+           let: "s1" := recv "ch2" #A in
+           (* <(Send,A) @ (s3:Z) (c3:Z)> MSG #s3 {{ ⌜(s3 = a3 + b3 + c3)%Z⌝ }} ; *)
+           let: "s3" := "a3"+"b3"+"c3" in
+           send "ch2" #A "s3";;
+           (* <(Recv,B) @ (s2:Z)> MSG #s2 ; <(Send,B)> MSG #s3; *)
+           let: "s2" := recv "ch2" #B in
+           send "ch2" #B "s3";;
+           send "ch2" #D ("s1"+"s2"+"s3"));;
+     let: "res1" := recv "ch3" #A in
+     let: "res2" := recv "ch3" #B in
+     let: "res3" := recv "ch3" #C in
+     assert: ("res1" = "res2");;
+     assert: ("res2" = "res3").
 
 Section mpc_protocols.
   Context `{!heapGS Σ, chanG Σ}.
 
-  Notation A := 0.
-  Notation B := 1.
-  Notation C := 2.
 
 (* A :  *)
 (*   ![B] (a2) <a2> . ![C] (a3) <a3> .  *)
@@ -34,11 +103,11 @@ Section mpc_protocols.
 
   Definition B_prot (p : Z → iProto Σ) : iProto Σ :=
     (<(Recv,A) @ (a2:Z)> MSG #a2 ; <(Send,A) @ (b1:Z)> MSG #b1 ;
-     <(Send,C) @ (c2:Z)> MSG #c2 ; <(Recv,C) @ (b3:Z)> MSG #b3 ;
+     <(Send,C) @ (b3:Z)> MSG #b3 ; <(Recv,C) @ (c2:Z)> MSG #c2 ;
      <(Recv,A) @ (s1:Z)> MSG #s1 ;
      <(Send,A) @ (s2:Z) (b2:Z)> MSG #s2 {{ ⌜(s2 = a2 + b2 + c2)%Z⌝ }} ;
      <(Send,C)> MSG #s2 ; <(Recv,C) @ (s3:Z)> MSG #s3 ;
-     p (s1 + s2 + s3)%Z)%proto. 
+     p (s1 + s2 + s3)%Z)%proto.
 
 (* C : *)
 (*   ?[A] (a3) <a3> . ![A] (c1) <c1> . *)
@@ -50,27 +119,85 @@ Section mpc_protocols.
   Definition C_prot (p : Z → iProto Σ) : iProto Σ :=
     (<(Recv,A) @ (a3:Z)> MSG #a3 ; <(Send,A) @ (c1:Z)> MSG #c1 ;
      <(Recv,B) @ (b3:Z)> MSG #b3 ; <(Send,B) @ (c2:Z)> MSG #c2 ;
-     <(Recv,A) @ (s1:Z)> MSG #s1 ; 
+     <(Recv,A) @ (s1:Z)> MSG #s1 ;
      <(Send,A) @ (s3:Z) (c3:Z)> MSG #s3 {{ ⌜(s3 = a3 + b3 + c3)%Z⌝ }} ;
-     <(Recv,B) @ (s2:Z)> MSG #s2 ; <(Send,B)> MSG #s3; 
-     p (s1 + s2 + s3)%Z)%proto.  
+     <(Recv,B) @ (s2:Z)> MSG #s2 ; <(Send,B)> MSG #s3;
+     p (s1 + s2 + s3)%Z)%proto.
 
   Notation D := 3.
 
   Definition D_prot : iProto Σ :=
     (<(Recv,A) @ (sum : Z)> MSG #sum ; <(Recv,B)> MSG #sum; <(Recv,C)> MSG #sum; END)%proto.
-    
+
   Definition tail_prot (sum : Z) : iProto Σ :=
      (<(Send,D)> MSG #sum ; END)%proto.
 
   Definition mpc_prot_pool : list (iProto Σ) :=
      [A_prot tail_prot; B_prot tail_prot; C_prot tail_prot; D_prot].
-  
+
   Lemma mpc_consistent :
     ⊢ iProto_consistent mpc_prot_pool.
   Proof.
     rewrite /mpc_prot_pool.
     iProto_consistent_take_steps.
+  Qed.
+
+(* End mpc_protocols. *)
+
+(* Section mpc_example. *)
+(*   Context `{!heapGS Σ, chanG Σ}. *)
+
+  Lemma mpc_spec :
+    {{{ True }}} program #() {{{ RET #(); True }}}.
+  Proof.
+    iIntros (Φ) "_ HΦ".
+    wp_lam.
+    wp_new_chan mpc_prot_pool with mpc_consistent
+      as (ch0 ch1 ch2 ch3) "Hc0" "Hc1" "Hc2" "Hc3".
+    wp_smart_apply (wp_fork with "[Hc0]").
+    { iIntros "!>".
+      wp_send with "[//]".
+      wp_send with "[//]".
+      wp_recv (b1) as "_".
+      wp_recv (c1) as "_".
+      wp_send with "[//]".
+      wp_send with "[//]".
+      wp_recv (s2) as "_".
+      wp_recv (s3) as "_".
+      wp_send with "[//]".
+      done. }
+    wp_smart_apply (wp_fork with "[Hc1]").
+    { iIntros "!>".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      wp_send with "[//]".
+      wp_recv (?) as "_".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      wp_send with "[//]".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      done. }
+    wp_smart_apply (wp_fork with "[Hc2]").
+    { iIntros "!>".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      wp_recv (?) as "_".
+      wp_send with "[//]".
+      wp_send with "[//]".
+      done. }
+    wp_recv (?) as "_".
+    wp_recv as "_".
+    wp_recv as "_".
+    wp_smart_apply wp_assert. wp_pures.
+    iSplitR; [by case_bool_decide|]. iIntros "!>!>".
+    wp_smart_apply wp_assert. wp_pures.
+    iSplitR; [by case_bool_decide|]. iIntros "!>!>".
+    by iApply "HΦ".
   Qed.
 
 End mpc_protocols.
